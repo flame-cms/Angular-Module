e01fc1c37b052c3c4f88d2f1f3826e120bd3ba32
JSifalda <sifalda.jiri@gmail.com>
2013-07-18 18:56:08 +0200

    Move into DI namespace

--- Flame/CMS/AngularModule/DI/AngularExtension.php
+++ Flame/CMS/AngularModule/DI/AngularExtension.php
@@ -5,7 +5,7 @@
  * @author: Jiří Šifalda <sifalda.jiri@gmail.com>
  * @date: 28.04.13
  */
-namespace Flame\CMS\AngularModule;
+namespace Flame\CMS\AngularModule\DI;
 
 use Flame\Modules\Extension\ModuleExtension;
 use Flame\Modules\Providers\IConfigProvider;
