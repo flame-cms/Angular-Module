6878c2d61a602e5f26bcac1111c6dff7cecd40ff
JSifalda <sifalda.jiri@gmail.com>
2013-07-25 10:59:45 +0200

    Remove config.neon, configure from extension

--- Flame/CMS/AngularModule/DI/AngularExtension.php
+++ Flame/CMS/AngularModule/DI/AngularExtension.php
@@ -8,18 +8,33 @@
 namespace Flame\CMS\AngularModule\DI;
 
 use Flame\Modules\Extension\NamedExtension;
-use Flame\Modules\Providers\IConfigProvider;
+use Flame\Modules\Providers\ILatteMacrosProvider;
+use Flame\Modules\Providers\IPresenterMappingProvider;
 
-class AngularExtension extends NamedExtension implements IConfigProvider
+class AngularExtension extends NamedExtension implements ILatteMacrosProvider, IPresenterMappingProvider
 {
 
 	/**
+	 * Get array of latte macros classes
+	 *
 	 * @return array
 	 */
-	public function getConfigFiles()
+	public function getLatteMacros()
 	{
 		return array(
-			$this->getConfigPath('config')
+			'Flame\CMS\AngularModule\Templating\AngularMacros'
 		);
 	}
-}
+
+	/**
+	 * Returns array of ClassNameMask => PresenterNameMask
+	 *
+	 * @see https://github.com/nette/nette/blob/master/Nette/Application/PresenterFactory.php#L138
+	 * @return array
+	 */
+	public function getPresenterMapping()
+	{
+		return array(
+			'Angular' => 'Flame\CMS\AngularModule\*Module\Presenters\*Presenter'
+		);
+	}}
